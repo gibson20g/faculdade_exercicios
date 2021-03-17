44074e3718299a35bda63161b09254ee486a54f3
gibson20g <77676359+gibson20g@users.noreply.github.com>
2021-03-17 18:29:31 -0300

    teste de atualização

---
+++ numeros_em_moda.py
@@ -0,0 +1,5 @@
+moda = 0
+while True:
+	num = int(input('Digite um Numero inteiro entre 0 e 10'))
+	if num == -1:
+		break
--- pessoas_menor_q_20_anos.py
+++ pessoas_menor_q_20_anos.py
@@ -1,13 +1,11 @@
-menor_20 = maior_50 = 0
-
+menor_21 = maior_50 = 0
 while True:
 	idade = int(input("Digite as idades: "))
-	if idade < 20:
-		menor_20 += 1
+	if idade < 21:
+		menor_21 += 1
 	if idade > 50:
 		maior_50 += 1
 	if idade == -99:
 		break
-
-print("Pessoas menor que 20: {} Pessoas ".format(menor_20))
+print("Pessoas menor que 20: {} Pessoas ".format(menor_21))
 print("Maior que 50: {} Pessoas ".format(maior_50))
